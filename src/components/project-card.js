a47f47603924fe1dafb31b177ab2e8e7c051d4a9
Zemi <43094930+Ucaninek@users.noreply.github.com>
2022-08-11 21:02:42 +0300

    added image viewer

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,11 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
+import Viewer from 'react-viewer';
 import Tag from './tag.js';
+import { useState } from 'react';
 
 function ProjectCard({ title, desc, github, image, tags }) {
     var git = '';
     if (github !== undefined) {
         git = github;
     }
+    const [visible, setVisible] = useState(false);
     return (
         <div className="project-container" key={uuidv4().toString()}>
             <div className="bg-gray-100 px-8 py-10 rounded-md grow shrink">
@@ -18,7 +21,19 @@ function ProjectCard({ title, desc, github, image, tags }) {
                     }
                 </div>
                 <div className="max-w-lg">
-                    <img src={image} alt={title} className="mb-8" />
+                    <img src={image} alt={title} className="mb-8" onClick={() => { setVisible(true); }} />
+                    <div>
+                        <Viewer
+                            visible={visible}
+                            rotatable={false}
+                            scalable={false}
+                            changeable={false}
+                            zoomable={false}
+                            drag={false}
+                            onClose={() => { setVisible(false); }}
+                            images={[{ src: image, alt: title }]}
+                        />
+                    </div>
                 </div>
                 <p className="font-normal text-gray-500 text-md">{desc}</p>
             </div>
